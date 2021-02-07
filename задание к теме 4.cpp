72adfca1e98a3172178ba1b310699bedfa7713a5
skillsskill <77679244+skillsskill@users.noreply.github.com>
2021-02-07 16:48:31 +0300

    Add files via upload

---
+++ задание к теме 4.cpp
@@ -0,0 +1,80 @@
+// ЗД 1 - стороны прямоугольника 
+#include < iostream>
+
+int main() {
+    int a=0,b=0;
+    std::cout << "a, b:";
+    std::cin >> a >> b;
+    std::cout << "S = " << a * b << std::endl
+    std::cout << "P = " << 2 * (a + b) << std::endl;
+    system("pause");
+    return 0;
+}
+
+//Зд 2 - диаметр окружности 
+
+#include<stdio.h>
+#include<locale.h>
+
+int main(){
+    int d;
+    float L, Pi = 3.14
+    printf("6:");
+    scanf_s("%d", &d);
+    L = Pi * d;
+    printf("18,84=%.2f\n", L);
+    getchar();
+    getchar();
+    return 0;
+
+  
+}
+
+//Зд 3 - два числа
+
+#include <iostream>
+
+int main(){
+    class Average{
+        private:
+           int a_;
+           int b_;
+        public:
+        Average(int a, int b,) : a_(a), b_(b){
+            double getAverage () const{
+                retyrn static_cast<double>(a_+b_) / 2;{
+                    void setValues(int a, int b,){
+                        a_ = a;
+                        b_ = b;
+                    }
+                }
+            }
+        }
+    }
+
+};
+int main(){
+    Average av(10, 20);
+    std::cout << av.getAverage() << std::endl;
+    av.setValues(23,22);
+    std::cout << av.getAverage() << std::endl;
+    returne 0,
+}
+
+//Зд 4 - два ненулевых числа
+
+#include <iostream>
+
+int main(){
+    int x = 1, y = 2;
+    std::cout << (x + y) << std::endl;
+    std::cout << (x - y) << srd::endl;
+    std::cout << (x * y) << std::endl;
+}
+
+//Зд 5 - два ненулевых числа
+
+
+
+// Не понял как делать 
+ 

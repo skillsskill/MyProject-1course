66498ccabe8e51076d6566a49384d0f853e76172
skillsskill <77679244+skillsskill@users.noreply.github.com>
2021-02-07 16:55:51 +0300

    Update задание к теме 4.cpp

--- задание к теме 4.cpp
+++ задание к теме 4.cpp
@@ -1,3 +1,4 @@
+https://github.com/skillsskill/MyProject-1course/blob/main/%D0%B7%D0%B0%D0%B4%D0%B0%D0%BD%D0%B8%D0%B5%20%D0%BA%20%D1%82%D0%B5%D0%BC%D0%B5%204.cpp
 // ЗД 1 - стороны прямоугольника 
 #include < iostream>
 

dd61c7f50649f94fe52f272723a3def21dc0e4d9
skillsskill <77679244+skillsskill@users.noreply.github.com>
2021-03-04 11:22:01 +0300

    Add files via upload

---
+++ Лаб.раб 7.cpp
@@ -0,0 +1,102 @@
+// https://github.com/skillsskill/MyProject-1course
+// ЗД 1 - не знаю
+// зд 2 - не знаю 
+
+//ЗД 3
+
+#include
+
+using namespace std;
+
+int main (){
+    float x, y, yn, o;
+    cout<< x - "4.400" << endl;
+    cin>>x;
+    cout<< "a - 425.48" << endl;
+    cin>>a;
+    0 = a/x;
+    cout<< "y - 4.900"<<endl;
+    cin>>en;
+    y = 0 * yn;
+    cout<< "1кг конфет = 96.70 "<<0<<endl;
+    cout<<yn<<"473.83 кг "<<y<<endl;
+    return 0;
+}
+
+//зд 4
+#include
+
+using namespace std;
+
+int main(){
+    float v,v1,s,s1,t;
+    cin>>v>>v1>>s>>t;
+    s1=(v*t)+(v1*t)+s;
+    cout<<s1;
+    return 0;
+}
+
+//зд 5
+
+#include<stdio.h>
+#include<stdlio.h>
+int showError(void){
+    printf("Wrong inputs");
+    exit(EXIT_FAILURE);
+}
+
+
+int main(){
+    double a,b;
+
+    printf("A, B:\n");
+    scanf("%1f %1f", &a, &b) == 2|| showError();
+    (a == 0) && showError();
+
+    printf("\nResult: %.4lf", -b / a);
+
+    getchar();
+    return 0;
+
+}
+
+
+//зд 6 
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    double a1=0, b1=0, c1=0, a2=0, b2=0, c2=0;
+    double D = 0;
+    double x = 0, y = 0;
+
+    cout << "a1 = 1 ";
+    cin >> a1;
+    cout << "b1 = 1 ";
+    cin >> b1;
+    cout << "с1 = 1 ";
+    cin >> c1;
+    cout << "a2 = 2 ";
+    cin >> a2;
+    cout << "b2 = 2 ";
+    cin >> b2 ;
+    cout << "c2 = 2 ";
+    cin >> c2;
+    
+    D = a1*ab - a2*b1;
+    if(D ! = 0){
+        x = (c1*b2 - c2*b1) / D;
+        y = (a1*c2 - a2*c1) / D;
+        cout << "\n x= "<< X << "y = "<<endl;
+    }
+    else{
+        cout << "a1*b2 - a2*b1 = 0" <<endl;
+    }
+    system("pause");
+    return 0;
+}
+
+
+

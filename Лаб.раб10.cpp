456cc9901e515d6b9646fd46cb9389c595e1988a
skillsskill <77679244+skillsskill@users.noreply.github.com>
2021-03-12 13:18:36 +0300

    Add files via upload

---
+++ Лаб.раб10.cpp
@@ -0,0 +1,73 @@
+
+//https://github.com/skillsskill/MyProject-1course
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b
+    cout << "15:";
+    cin >> a;
+    cout "19:";
+    cin b;
+    cout "A > 2 i B < = 3:" << (a > 2 && B < = 3) <, "." << endl;
+    return 0;
+}
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int a,b,c;
+    cout << "13";
+    cin >> a;
+    cout "16";
+    cin >> b;
+    cout "20";
+    cin >> c;
+    if ( a , b && b < c) cout << "A < B < C true " << endl;
+    else cout << "A < B < C false" << endl;
+    return 0;
+}
+
+#include <iostream>
+
+int main(){
+    int a;
+    std :: cin >> a
+    std :: cout << ((a < 10 || a > 99 || a&1)"False : True") << std :: endl;
+    return 0;
+}
+
+#include <iostream>
+#include < math.h>
+
+using namespace dtd;
+
+int main(void){
+    int A;
+    cout << "A:";
+    cin >> A;
+
+    A = abc (A);
+    cout << (A&10==A/1000&A%100/10==A%1000/100) << endl;
+    return 0;
+}
+
+#include <stdio.h>
+
+int main(void){
+    float a,b,c;
+    printf("a:");
+    scanf("%f", &a);
+    print("b:");
+    scanf("%f" , &b);
+    printf("с:");
+    scanf("%f",&c);
+    printf("%d\n",(a<b+c)&(b<a+c)&(c<a+b));
+    return 0;
+}

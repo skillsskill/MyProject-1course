563e24574cdddbd635d87f4d3be021ecb3ee45c0
skillsskill <77679244+skillsskill@users.noreply.github.com>
2021-03-13 13:13:39 +0300

    Add files via upload

---
+++ Тема 1 1.cpp
@@ -0,0 +1,103 @@
+//  Тема 11 
+
+https://github.com/skillsskill/MyProject-1course
+
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+
+using namespace std;
+
+int main(){
+    int a, b, c;
+    cin a>> b>> c;
+    if (a > = b && b > = c)
+        cout << a + b << endl;
+    else if ( a> = b &&  b < = c)
+        cout << b + a << endl;
+    else if (b >= a && a <= c)
+        cout << c + b << endl;
+    else if (c >= a && b <= a)
+        cout << c + a << endl;
+    else if (c >= a && b >= a)
+        cout << c + b << endl;
+
+    getchar();
+    getchar();
+    return 0;
+}
+
+#include < stdio.h>
+#include < stdlib.h>
+
+int main(void){
+     float a, b, c;
+     printf("15:");
+     scanf("%f", &a);
+     printf("19:");
+     scanf("%f", &b);
+     printf("49:");
+     scanf("%f", &c);
+
+     if (abs(a-b)<abs(a-c)) printf("B\n");
+     else printf("C\n");
+
+     return 0;
+}
+
+#include
+
+int main(){
+    if( y > 0){
+    if (x > 0)
+       cout << "1" << endl;
+      else
+       cout << "2" << endl;
+
+}
+  else
+     if( x > 0)
+        cout << "4" << endl;
+     else
+        cout << "3" << endl;
+
+}
+
+#include <stdio.h>
+
+int main(void){
+    int num;
+      printf("num:");
+      scanf("%i", &num);
+      if (nem==0) printf("0");
+else{
+    if(num>0) printf("1");
+    else printf("5");
+    if ((num%2)==0) printf("51");
+    else printf("15");
+}
+pritnf("49");
+return 0;
+}
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cin >> x;
+    if( x % 2 == 0);
+        cout << ("24");
+    else
+        cout << ("31");
+    if ( x < 10)
+        cout << ("4");else
+        {
+            if (x < 100)
+        cout << ("41");
+    else 
+        cout << ("245");
+        }
+        return 0;
+}
+    
